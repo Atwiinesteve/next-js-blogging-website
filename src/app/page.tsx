1c1eb385c15e2f20a0efc00b43a3d47c6df27dc2
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2024-02-13 17:20:51 +0300

    Add Tags component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Blogs from "./components/Blogs";
 import Header from "./components/Header";
 import { NextUIProvider } from "@nextui-org/react";
+import Tags from "./components/Tags";
 
 export default function Home() {
 	return (
@@ -10,6 +11,7 @@ export default function Home() {
 			<NextUIProvider>
 				<Header />
 				<Blogs />
+				<Tags />
 			</NextUIProvider>
 		</div>
 	);
