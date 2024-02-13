145b9a06d29e8ac69ced86202294db1128d42c77
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2024-02-13 18:24:52 +0300

    Add OtherBlogs component and update Tags component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			
-				<body className={manrope.className}>
+				<body className={`${manrope.className}`}>
 					<UpperNavbar />
 					<MainNavbar />
 					{children}
